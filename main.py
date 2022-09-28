d07f862d4b571677b7742222783bd8bbed95531a
Moises Rodriguez <113930437+Msrodriguez17@users.noreply.github.com>
2022-09-28 17:37:27 +0000

    importing math and finding new numbers

--- main.py
+++ main.py
@@ -14,15 +14,16 @@
 
 seconds = [1.23, 1.45, 1.02]
 current = 1.11
-
+seconds.append(current)
+print(seconds)
 # Remove item 1.45 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
-
-
+seconds.remove(1.45)
+print(seconds)
 # Remove items 1.45, 1.02, and 1.11 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
 
-
+seconds
 ################################comparison operators#########################
 #remember....
 # > greater
@@ -33,11 +34,33 @@ seconds = [1.23, 1.45, 1.02, 1.11]
 # != different or not equal to
 
 
+#lesson
+my_bool = 10 == 25
+print(my_bool)#false
 
+my_bool = 5 + 5 == 10 - 0
+print(my_bool)#true
+
+my_bool = 'white' == 'White'.lower()
+print(my_bool)#false due to letter  #made true due to lower()
+
+my_bool = 4 < 5 and 5 >= 6
+print(my_bool)
+
+my_bool = (55 == 55) and (5 == 2 + 3)
+print(my_bool)
+
+text = "this sentence is short"
+#my_bool = ('sentence' in text) and ('short' in text)
+my_bool = ('sentence' in text) or ('python' in text)
+print(my_bool)
 
 # Comparison Operators Practice  1:
 # Create two variables (num1 and num2) with the following values: 36 and 17. Check if num1 is greater than or equal to num2 and store the result of that comparison in a variable called my_bool
-
+num1 = 36
+num2 = 17
+my_bool = num1 >= num2
+print(my_bool)
 
 
 # Comparison Operators Practice  2:
@@ -46,6 +69,10 @@ seconds = [1.23, 1.45, 1.02, 1.11]
 # Inside num2, store the number 5.
 # Check if num1 is equal to num2 and store the result of that comparison in a variable called my_bool.
 
+from math import *
+num1 = 5
+print(sqrt(25))
+num2 = 5
 
 # Comparison Operators Practice #3:
 # Create two variables (num1 and num2):
